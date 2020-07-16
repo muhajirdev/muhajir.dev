9679f02ba033768e9b126ca151c9abbbcadf46b8
Muhammad Muhajir <muhajirframe@gmail.com>
2020-07-16 08:41:20 +0700

    markdown boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,47 @@
-import React from 'react'
+import matter from 'gray-matter'
 
-export default () => <div className="bg-red-200">Hello world</div>
+import Layout from '@components/layout'
+import PostList from '@components/postlist'
+
+const index = ({ title, description, posts, ...props }) => {
+  return (
+    <Layout pageTitle={title}>
+      <h1 className="title">Welcome to my blog!</h1>
+      <p className="description">{description}</p>
+      <main>
+        <PostList posts={posts} />
+      </main>
+    </Layout>
+  )
+}
+
+export default index
+
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`)
+
+  const posts = ((context) => {
+    const keys = context.keys()
+    const values = keys.map(context)
+
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
+      const value = values[index]
+      const document = matter(value.default)
+      return {
+        frontmatter: document.data,
+        markdownBody: document.content,
+        slug,
+      }
+    })
+    return data
+  })(require.context('../posts', true, /\.md$/))
+
+  return {
+    props: {
+      posts,
+      title: configData.default.title,
+      description: configData.default.description,
+    },
+  }
+}
