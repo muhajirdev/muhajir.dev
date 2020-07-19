4016cb5a80de2b1388e3a50e3b09b6281f1c8d2e
Muhammad Muhajir <muhajirframe@gmail.com>
2020-07-19 15:49:52 +0700

    flex-wrap postlist

--- components/postlist.js
+++ components/postlist.js
@@ -8,7 +8,7 @@ export default function PostList({ posts }) {
   return (
     <div>
       {posts.length <= 0 && <div>No posts!</div>}
-      <div className="flex -mx-8">
+      <div className="flex -mx-8 flex-wrap">
         {posts &&
           posts.map((post) => {
             const postSlug = slug(post.title)
@@ -18,7 +18,7 @@ export default function PostList({ posts }) {
                 as={`/post/${post.number}/${postSlug}`}
                 key={postSlug}
               >
-                <a className="mb-4 shadow-md rounded-md h-64 p-6 w-64 mx-8 flex flex-col justify-between">
+                <a className="mb-8 shadow-md rounded-md h-64 p-6 w-64 mx-8 flex flex-col justify-between">
                   <h2 className="text-xl tracking-wide font-bold">
                     {post.title}
                   </h2>
