d0449d4e70f4981d17b862738495da5bdddc1266
Muhammad Muhajir <muhajirframe@gmail.com>
2020-07-19 15:36:01 +0700

    add footer

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,24 @@
 export default function Footer() {
   return (
-    <div className="flex justify-center py-4">
-      <div>
-        This site is built with <a href="https://nextjs.org/">NextJS</a>, hosted
-        on <a href="https://netlify.com">Netlify</a>, and uses{' '}
-        <a href="https://github.com/muhajirdev/muhajir.dev/issues">
-          Github Issues{' '}
-        </a>{' '}
-        as CMS
-      </div>
+    <div className="flex flex-col items-center py-4">
+      <Social />
+      <Tech />
     </div>
   )
 }
+const Social = () => (
+  <div>
+    Reach me on twitter <a href="https://twitter.com/muhajirdev">@muhajirdev</a>
+  </div>
+)
+
+const Tech = () => (
+  <div>
+    This site is built with <a href="https://nextjs.org/">NextJS</a>, hosted on{' '}
+    <a href="https://netlify.com">Netlify</a>, and uses{' '}
+    <a href="https://github.com/muhajirdev/muhajir.dev/issues">
+      Github Issues{' '}
+    </a>{' '}
+    as CMS
+  </div>
+)
