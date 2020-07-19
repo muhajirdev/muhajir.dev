9de083b534917584d9f0114c08e3a22bcdea9ea1
Muhammad Muhajir <muhajirframe@gmail.com>
2020-07-19 15:11:57 +0700

    fix bug image not showing up

--- pages/post/[...post].js
+++ pages/post/[...post].js
@@ -2,8 +2,13 @@ import Layout from '@components/layout'
 import { getPost, getPosts } from 'lib/github-graphql-client'
 import slug from 'slug'
 
-import rehype from 'rehype'
+import markdown from 'remark-parse'
+import images from 'remark-images'
+import markdown2rehype from 'remark-rehype'
+import unified from 'unified'
 import highlight from 'rehype-highlight'
+import html from 'rehype-stringify'
+import raw from 'rehype-raw'
 
 export default function BlogPost({ title, bodyHTML }) {
   return (
@@ -25,12 +30,16 @@ export async function getStaticProps({ ...ctx }) {
   const [id, slug] = postParam
 
   const post = await getPost(id)
-  console.log('bodyHTML', post.bodyHTML)
+  console.log('bodyHTML', post.body)
 
-  const reHTML = await rehype()
-    .data('settings', { fragment: true })
+  const reHTML = await unified()
+    .use(markdown)
+    .use(images)
+    .use(markdown2rehype, { allowDangerousHtml: true })
+    .use(raw)
     .use(highlight)
-    .process(post.bodyHTML)
+    .use(html)
+    .process(post.body)
 
   return {
     props: {
